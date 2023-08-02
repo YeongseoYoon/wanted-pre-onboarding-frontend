00cd09d83fcd27d9fa342a90549bad3578409103
Eagerbeaver <86523545+YeongseoYoon@users.noreply.github.com>
2023-08-02 20:04:15 +0900

    :lipstick: 레이아웃 설정
    레이아웃 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import { Outlet } from 'react-router-dom';
-
+import { Outlet } from "react-router-dom";
+import Header from "./components/Header";
+import Layout from "./components/Layout";
 
 function App() {
   return (
-    <>
+    <Layout>
       <Header />
       <Outlet />
-    </>
+    </Layout>
   );
 }
 
